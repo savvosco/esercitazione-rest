a2d8b54824401c2ced80c1280af6d1c8e17d4c4c
Sandbox <test@test.com>
2023-05-21 11:05:06 +0200

    added project

---
+++ esercitazione-logs-props/src/main/java/com/example/apulia/javaee/logsprops/LogspropsApplication.java
@@ -0,0 +1,13 @@
+package com.example.apulia.javaee.logsprops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogspropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogspropsApplication.class, args);
+	}
+
+}
---
+++ esercitazione-logs-props/src/test/java/com/example/apulia/javaee/logsprops/LogspropsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apulia.javaee.logsprops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogspropsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
